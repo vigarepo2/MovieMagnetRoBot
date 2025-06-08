a2777622c616f6988a54892f4eee9b0624d74ed5
Viga Bots <133951124+vigarepo2@users.noreply.github.com>
2025-06-08 14:43:54 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,14 @@
 
-if [ -z $UPSTREAM_REPO ]
+if [ -z "$UPSTREAM_REPO" ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Joelkb/DQ-the-file-donor.git /DQTheFileDonorBot
+  git clone https://github.com/vigarepo2/MovieMagnetRoBot.git /MovieMagnetRoBot
 else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQTheFileDonorBot
+  echo "Cloning Custom Repo from $UPSTREAM_REPO"
+  git clone $UPSTREAM_REPO /MovieMagnetRoBot
 fi
-cd /DQTheFileDonorBot
+cd /MovieMagnetRoBot
 pip3 install -U -r requirements.txt
-echo "Starting DQ-The-File-Donor...."
+
+echo "Starting MovieMagnetRoBot...."
 python3 bot.py
